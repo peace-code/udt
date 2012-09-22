3dd6d4a19357631900af5850ec87df8f82097342
KWEON Ohyeon <rest515@gmail.com>
2012-09-22 13:27:51 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Udt::Application.config.secret_token = '21349d1c95eba4523918016f2ce89a6b641977f2bbe3d4e999baad36b796cbc2d723d14ce9a3d9bcab6c3f5501019562e4b2b51a16f5fc6922e883694deb97cb'
